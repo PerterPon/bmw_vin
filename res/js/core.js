863f45b0bed46cda1d2348cf804d0b86b802cd5a
梁月 <yuhan.wyh@alibaba-inc.com>
2015-09-25 16:03:11 +0800

    add force update function

--- res/js/core.js
+++ res/js/core.js
@@ -11,6 +11,17 @@ $( function() {
 
   var needSecurityCode = false;
 
+  $( 'body' ).on( 'click', '.force_update', function() {
+    needForceUpdate();
+    window.alert( '请输入验证码，然后从官方地址重新获取生产日期' );
+    $( '.result-box' ).html( '' );
+  } );
+
+  /**
+   * [render 渲染列表]
+   * @param  {[type]} data [description]
+   * @return {[type]}      [description]
+   */
   function render( data ) {
     $( '.result-box' ).html( '' );
     for( var item in data ) {
@@ -25,10 +36,27 @@ $( function() {
         } else {
           content   = tr.name || tr.en_name;
         }
-        $table.append( '<tr><td>' + ( tr.id || tr.en_id ) + '</td><td>' + content + '</td></tr>' );
+        var enId = tr.id || tr.en_id;
+        var $td  = $( '<tr><td>' + ( enId ) + '</td><td>' + content + '</td></tr>' );
+        if( '生产日期' === enId && '0001-01-01' === content.trim() ) {
+          $td.find( 'td:eq( 1 )' ).append( '<button class="btn btn-info force_update">强制更新</button>' );
+        }
+        $table.append( $td );
       }
       $( '.result-box' ).append( $table );
     }
+  }
+
+  /**
+   * [needForceUpdate 强制更新]
+   * @return {[type]} [description]
+   */
+  function needForceUpdate() {
+    needSecurityCode = true;
+    $( '.securitycode' ).append( '<iframe id="secury_code_iframe" name="secury_code_iframe" src="/rep.html?3333"></iframe>' );
+    $( '#secury_code_iframe' ).on( 'load', function() {
+      $( '.loadding', document.secury_code_iframe.document ).remove();
+    } );
   }
 
   $( '.btn-default' ).on( 'click', function() {
@@ -37,7 +65,7 @@ $( function() {
       var recaptcha_response_field  = $( '#recaptcha_response_field', document.secury_code_iframe.document ).val();  
     }
     
-    var vin_number                = $( '#vin_number' ).val();
+    var vin_number                  = $( '#vin_number' ).val();
 
     if( 7 !== vin_number.length ) {
       return alert( '车架号位数不对, 请输入后7位车架号' );
@@ -73,11 +101,7 @@ $( function() {
         var status = res.status;
         $( '.loading' ).remove();
         if( 404 === status ) {
-          needSecurityCode = true;
-          $( '.securitycode' ).append( '<iframe id="secury_code_iframe" name="secury_code_iframe" src="/rep.html?3333"></iframe>' );
-          $( '#secury_code_iframe' ).on( 'load', function() {
-            $( '.loadding', document.secury_code_iframe.document ).remove();
-          } );
+          needForceUpdate();
           window.alert( '暂未在本地查询到车架号信息, 请输入验证码从官方地址获取' );
         } else {
           $( '.result-box' ).html( '<div class="error">查询超时, 当前查询通道拥挤, 请稍后重试</div>' );  
